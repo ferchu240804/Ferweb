0501fdde04c2f35c9b8362757f27d644fc13c02e
ferchu240804 <fersecundario2404@gmail.com>
2024-12-30 11:01:51 -0300

    Add files via upload

--- iniciosesion.prueba.js
+++ iniciosesion.prueba.js
@@ -25,14 +25,18 @@
   
       const usuarioValido = usuarios.find(u => u.username === loginUsername && u.password === loginPassword);
       if (usuarioValido) {
- // Mostrar mensaje grande y reiniciar la página
- document.body.innerHTML = `<div style="text-align:center; margin-top: 20%; font-size: 2em; font-weight: bold; color: green;">
- Gracias por probar esta página 😀
-</div>`;
-setTimeout(() => {
- location.reload(); // Reinicia la página después de 3 segundos
-}, 3000);
-} else {
-alert('Usuario o contraseña incorrectos.');
-}
+ // Mostrar un mensaje de bienvenida por unos segundos
+      document.body.innerHTML = `<div style="text-align:center; margin-top: 20%; font-size: 2em; font-weight: bold; color: green;">
+    ¡Bienvenido, ${loginUsername}! 😀
+    </div>`;
+
+// Recargar la página después de un retraso de 2 segundos
+      setTimeout(function() {
+     location.reload();  // Recarga la página actual
+    }, 2000);  // Espera 2 segundos antes de recargar
+
+} else {    
+    alert('Usuario o contraseña incorrectos.');
+  }
+  
 });
